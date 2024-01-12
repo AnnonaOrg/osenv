2f9a5a2b5975d1b3ff388129e4142d03019ca932
noreply <noreply@yourdomain.com>
2024-01-12 14:07:24 +0800

    first commit

---
+++ bot_env.go
@@ -0,0 +1,43 @@
+package osenv
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+// manager id
+func GetBotManagerID() string {
+	return os.Getenv("BOT_MANAGER_ID")
+}
+func IsBotManagerID(id int64) bool {
+	return strings.EqualFold(
+		GetManagerID(),
+		fmt.Sprintf("%d", id),
+	)
+}
+
+// Bot token
+func GetBotTelegramToken() string {
+	return os.Getenv("BOT_TELEGRAM_TOKEN")
+}
+
+// bot webhook url
+func GetBotTelegramWebhookURL() string {
+	return os.Getenv("BOT_TELEGRAM_WEBHOOK_URL")
+}
+
+// 告警通知群id: 接收反馈消息的Chat id
+func GetBotReportChatID() string {
+	return os.Getenv("BOT_REPORT_CHAT_ID")
+}
+
+// welecome msg
+func GetBotWelcomeMsg() string {
+	return os.Getenv("BOT_WELCOME_MSG")
+}
+
+// 文本消息尾巴
+func GetBotCaptionExt() string {
+	return os.Getenv("BOT_CAPTION_EXT")
+}
