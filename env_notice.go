f47b8af305ef90e2ccc1e987de5ecaf7053f7f2c
noreply <noreply@yourdomain.com>
2024-01-15 10:46:03 +0800

    debug

--- env_notice.go
+++ env_notice.go
@@ -4,12 +4,12 @@ import (
 	"os"
 )
 
-// notice of feedRichMsg push url like   https://xxx.domain
+// notice of feedRichMsg push url like   https://server.domain
 func GetNoticeOfFeedRichMsgPushUrl() string {
 	return os.Getenv("NOTICE_OF_FEEDRICHMSG_PUSH_URL")
 }
 
 // notice of feedRichMsg push url path like   /ws/push/
 func GetNoticeOfFeedRichMsgPushUrlPath() string {
-	return os.Getenv("NOTICE_OF_FEEDRICHMSG_PUSH_URL_Path")
+	return os.Getenv("NOTICE_OF_FEEDRICHMSG_PUSH_URL_PATH")
 }
