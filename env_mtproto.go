47019f90c371b785f8ef554447d6bcb44ee4eef8
noreply <noreply@yourdomain.com>
2024-06-20 12:47:58 +0800

    debug

--- env_mtproto.go
+++ env_mtproto.go
@@ -2,6 +2,7 @@ package osenv
 
 import (
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -14,6 +15,15 @@ func GetMtprotoProxyServer() string {
 }
 func GetMtprotoProxyPort() string {
 	return os.Getenv("MTPROTO_PROXY_PORT")
+}
+func GetMtprotoProxyPortInt() int64 {
+	// return os.Getenv("MTPROTO_PROXY_PORT")
+	retText := os.Getenv("MTPROTO_PROXY_PORT")
+	retInt, err := strconv.ParseInt(retText, 10, 64)
+	if err != nil {
+		return 0
+	}
+	return retInt
 }
 func GetMtprotoProxySecret() string {
 	return os.Getenv("MTPROTO_PROXY_SECRET")
