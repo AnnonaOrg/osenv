a4cbe593089eed7cb82fc6ce49125b49b2c983ad
“dev” <dev@domain.name>
2025-05-21 12:11:14 +0800

    debug

--- env_server_db_redis.go
+++ env_server_db_redis.go
@@ -5,6 +5,8 @@ import (
 	"strconv"
 )
 
+// 主节点（可读写）
+
 // server db redis address
 // SERVER_DB_REDIS_ADDRESS 127.0.0.1:6379
 func GetServerDbRedisAddress() string {
@@ -21,6 +23,34 @@ func GetServerDbRedisPassword() string {
 // SERVER_DB_REDIS_DB 0 1 2 3
 func GetServerDbRedisDB() int {
 	v := os.Getenv("SERVER_DB_REDIS_DB")
+	if v == "" {
+		return 0
+	}
+	if vi, err := strconv.Atoi(v); err != nil {
+		return 0
+	} else {
+		return vi
+	}
+}
+
+// 从节点（用于读）
+
+// server db redis address
+// SERVER_DB_REDIS_ADDRESS 127.0.0.1:6379
+func GetServerDbRedisSlaveAddress() string {
+	return os.Getenv("SERVER_DB_REDIS_SLAVE_ADDRESS")
+}
+
+// server db redis password
+// SERVER_DB_REDIS_PASSWORD Redis密码
+func GetServerDbRedisSlavePassword() string {
+	return os.Getenv("SERVER_DB_REDIS_SLAVE_PASSWORD")
+}
+
+// server db redis Redis库
+// SERVER_DB_REDIS_DB 0 1 2 3
+func GetServerDbRedisSlaveDB() int {
+	v := os.Getenv("SERVER_DB_REDIS_SLAVE_DB")
 	if v == "" {
 		return 0
 	}
